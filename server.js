46eaff41af11a97c2042eead4d9c05f9efe64ad9
apple <apple@apples-MacBook-Pro.local>
2018-05-18 23:03:36 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+var app = express();
+app.use(express.static(__dirname+'/public'));
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+    res.render('home.hbs');
+});
+app.get('/',(req,res) => {
+    res.send("send");
+})
+app.get('/bad',(req,res) => {
+    res.send({
+        "erroMssg":"Bad Request"
+    });
+
+});
+app.get('/welcome',(req,res) => {
+    res.render('home.hbs',{
+        "pageTitle":"Bad Request",
+        "currentYear":new Date().getFullYear()
+    });
+
+})
+app.listen(3000);
