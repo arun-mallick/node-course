c32f9e5aec534f787e20c9865268874375747757
apple <apple@apples-MacBook-Pro.local>
2018-05-21 23:09:19 +0530

    changes committed

--- server.js
+++ server.js
@@ -1,15 +1,57 @@
 const express = require('express');
 const port = process.env.PORT || '3000';
+var hbs = require('hbs');
 var app = express();
-app.use(express.static(__dirname+'/public'));
+//app.use(express.static(__dirname+'/public'));
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getTime',() => {
+    return new Date();
+});
+hbs.registerHelper('upper',(text) => {
+    return text.toUpperCase();
+});
 app.set('view engine','hbs');
+app.get('/',(req,res) => {
+    res.render('home.hbs',{
+        pageTitle:"Handlebars",
+        welcomeMsg:"My website"
+    });
+})
+app.get('/help',(req,res) => {
+    res.render('help.hbs');
+})
+app.get('/home',(req,res) => {
+    res.render('home.hbs',{
+        pageTitle:"Handlebars",
+        welcomeMsg:"Welcome to My website"
+    });
+})
 
+app.listen(port);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 app.use((req,res,next) => {
     res.render('home.hbs');
     next();
 });
 app.get('/',(req,res) => {
-    res.send("send");
+    res.render('home.hbs');
 })
 app.get('/home',(req,res) => {
     res.render('home.hbs',{
@@ -23,6 +65,5 @@ app.get('/help',(req,res) => {
         "pageTitle":"Help",
         "currentYear":new Date().getFullYear()
     });
-
 })
-app.listen(port);
+*/ 
